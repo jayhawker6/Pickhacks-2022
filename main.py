a28bafb677b1aaec669ed731a20b2dd66f43948a
Andrew Guernsey <andrewgguernsey@gmail.com>
2022-04-09 20:32:03 -0500

    All good!

--- main.py
+++ main.py
@@ -21,38 +21,49 @@ def interest_calc():
                    sg.Input(key="principle_amount", change_submits=False, default_text=principle_amount), sg.Text('$')],
                   [sg.Text("Interest Rate (percent):"),
                    sg.Input(key="interest_rate", change_submits=False, default_text=interest_rate), sg.Text('%')],
-                  [sg.Text("Cycles:"), sg.Input(key="cycles", change_submits=False, default_text=time_periods)],
-                  [sg.Text("Total Interest: " + str(total_interest))],
-                  [sg.Button("Submit")]]
+                  [sg.Text("Cycles :"), sg.Input(key="cycles", change_submits=False, default_text=time_periods)],
+                  [sg.Text("Interest Amount: " + str(total_interest))],
+                  [sg.Button("Calculate Simple"), sg.Button("Calculate Compound per charge (after n cycles)")]]
         window = sg.Window("Interest Calculator", layout)
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == "Exit":
             raise FatalSystemError("User Initiated Exit")
-        elif event:
+        elif event == "Calculate Simple":
             try:
                 principle_amount = int(values["principle_amount"])
                 interest_rate = int(values["interest_rate"])
                 time_periods = int(values["cycles"])
-                total_interest = principle_amount * (interest_rate / 100) * time_periods
+                total_interest = principle_amount * ((interest_rate / 100) * time_periods)
             except ValueError:
                 pass
+        elif event == "Calculate Compound per charge (after n cycles)":
+            try:
+                accumulated = 0
+                principle_amount = int(values["principle_amount"])
+                interest_rate = int(values["interest_rate"])
+                time_periods = int(values["cycles"])
+                for i in range(time_periods):
+                    total_interest = (accumulated + principle_amount) * ((interest_rate / 100) * time_periods)
+                    accumulated += total_interest
+            except ValueError:
+                pass
+
         window.close()
         window.refresh()
 
 
 def viewfinances():
     def plotmoney():
-        realValue = money + debt
         mainchart = plt.pyplot
-        mainchart.plot(time, money)
-        mainchart.plot(time, debt)
-        mainchart.plot(time, realValue)
+        mainchart.plot(df[money_choice])
+        mainchart.plot(df[debt_choice])
+        mainchart.plot(df['realValue'])
+        mainchart.plot(df[SMA])
         mainchart.grid(True)
         mainchart.ylabel('Amount of Money (in dollars)', loc='center')
         mainchart.xlabel('Chronological Order')
         mainchart.title('Finances')
-        mainchart.legend(['Money', 'Debt', 'Actual Value'])
-        mainchart.ylim()
+        mainchart.legend(['Money', 'Debt', 'Actual Value', 'Moving Average'])
         mainchart.show()
 
     layout = [[sg.T("")],
@@ -72,12 +83,45 @@ def viewfinances():
 
     window.close()
 
-    df = pd.read_csv(file, header=0)
+    layout = [[sg.T("")],
+              [sg.Text("What would you like to do?")],
+              [sg.Button("person_1"), sg.Button("person_2"), sg.Button("person_3"), sg.Button("person_4")]]
+    chooseuserwindow = sg.Window("Who are you?", layout)
+    while True:
+        event, values = chooseuserwindow.read()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            raise FatalSystemError("User Initiated Exit")
+        elif event == 'person_1':
+            money_choice = 'Money'
+            debt_choice = "Debt"
+            SMA = "SMA"
+        elif event == 'person_2':
+            money_choice = 'Money2'
+            debt_choice = "Debt2"
+            SMA = "SMA2"
+        elif event == 'person_3':
+            money_choice = 'Money3'
+            debt_choice = 'Debt3'
+            SMA = "SMA3"
+        elif event == 'person_4':
+            money_choice = 'Money4'
+            debt_choice = 'Debt4'
+            SMA = "SMA4"
+        else:
+            money_choice = 'Money'
+            debt_choice = "Debt"
+            SMA = "SMA"
+        window.close()
+        break
 
-    money = df['Money']
-    debt = df['Debt']
-    time = df['Time']
-
+    df = pd.read_csv(file, index_col='Time')
+    df['SMA'] = df['Money'].rolling(5).mean()
+    df['SMA2'] = df['Money2'].rolling(5).mean()
+    df['SMA3'] = df['Money3'].rolling(5).mean()
+    df['SMA4'] = df['Money4'].rolling(5).mean()
+    money = df[money_choice]
+    debt = df[debt_choice]
+    df['realValue'] = money + debt
     plotmoney()
 
 
